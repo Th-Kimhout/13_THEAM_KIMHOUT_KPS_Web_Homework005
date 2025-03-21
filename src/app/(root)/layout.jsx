18a993ab6385de250d2858e400282ff755621ac5
Th-Kimhout <th.kimhout@gmail.com>
2025-03-21 16:27:13 +0700

    add suspense

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -3,6 +3,7 @@ import SearchBarComponent from "./_component/SearchbarComponent";
 import { AppSidebar } from "./_component/SidebarComponent";
 import HeaderWrapperComponent from "./_component/HeaderWrapperComponent";
 import { Noto_Sans_Georgian } from "next/font/google";
+import { Suspense } from "react";
 const notoSansGeorgian = Noto_Sans_Georgian({
   variable: "--font-noto-sans-georgian",
   subsets: ["latin", "georgian"],
@@ -16,13 +17,15 @@ export default function RootLayout({ children }) {
           "--sidebar-width-mobile": "20rem",
         }}
       >
-        <AppSidebar  />
+        <AppSidebar />
         <SidebarTrigger className="!border-l-0" />
         <main className="flex-1 flex flex-col gap-6 pt-6 px-12 !bg-base-background">
           <SearchBarComponent />
           <section className="h-full bg-white pt-14 px-10 flex flex-col flex-1 rounded-tr-4xl rounded-tl-4xl">
             <div>
-              <HeaderWrapperComponent />
+              <Suspense fallback={<p>loading....</p>}>
+                <HeaderWrapperComponent />
+              </Suspense>
             </div>
             {children}
           </section>
